1c6fc4bc1a7deaa837cb77991408cda2ed9ddbe7
TuanDev <kutuanonline199@gmail.com>
2023-08-19 13:34:26 +0700

    add style button search

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,7 +1,9 @@
 import Alpine from "alpinejs";
+import Mousetrap from '@danharrin/alpine-mousetrap'
 import screen from "@victoryoalli/alpinejs-screen";
 
 Alpine.plugin(screen);
+Alpine.plugin(Mousetrap)
 
 window.Alpine = Alpine;
 
